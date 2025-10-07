bbb35e7581df57e834bc7e510dba7351c899958e
Peter Marki <marpeter@protonmail.com>
2025-10-07 10:42:58 +0200

    Add custom attributes

--- src/my-custom-element.ts
+++ src/my-custom-element.ts
@@ -1,9 +1,9 @@
-import templateHTML from './template.html?raw';
+import templateHTML from "./template.html?raw";
 
 class MyCustomElement extends HTMLElement {
   constructor() {
     super();
-    const shadow = this.attachShadow({mode: 'open'});
+    const shadow = this.attachShadow({mode: "open"});
 
     // Clone <template> element from html file and attach it to the shadow DOM.
     const templateDoc = new DOMParser().parseFromString(templateHTML, "text/html");
@@ -11,10 +11,22 @@ class MyCustomElement extends HTMLElement {
     shadow.appendChild(templateElement.content.cloneNode(true));
   }
 
+  static get observedAttributes() {
+    return ["greeting", "name"];
+  }
+
   connectedCallback() {
     // Left empty for this starter. This is where you would initialize your event listeners or
     // other logic on the elements inside the template.
     console.log("connected");
+  }
+
+  attributeChangedCallback(attr: string, _: string | null, newValue: string | null) {
+    // In this case I named the ids the exact same as the attributes, but you could map it any way
+    // you want.
+    if (MyCustomElement.observedAttributes.includes(attr)) {
+      this.shadowRoot!.getElementById(attr)!.textContent = newValue;
+    }
   }
 }
 
